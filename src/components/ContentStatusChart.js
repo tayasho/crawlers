33dda50abdbb59e7f26fc24e90ef3b8ed5426e71
tayasho <taya@osintech.net>
2025-05-02 15:33:53 +0900

    2025/05/02

--- src/components/ContentStatusChart.js
+++ src/components/ContentStatusChart.js
@@ -3,10 +3,10 @@ import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Responsive
 import './ContentStatusChart.css';
 
 const ContentStatusChart = () => {
-  const published = 1401;
-  const draft = 507;
+  const published = 1443;
+  const draft = 472;
   const archived = 561; // この値はサマリーに表示しませんが、グラフや他の計算で使われます
-  const stopped = 230;
+  const stopped = 281;
   const running = published-stopped;
 
   // ★ ユーザー指定の「全件」合計を計算
